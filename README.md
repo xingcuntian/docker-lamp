60c1b78e2687f9e16e334ff9af84c028a875f9a8
xingcuntian <xingcuntian@qq.com>
2017-08-08 11:25:11 +0800

    update

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 docker-compose
 
 docker exec -it mysql-db  /bin/bash
-
-sudo docker run --net=host  --name mysql-8.0   -e MYSQL_DATABASE=test -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -v $PWD/config/my.cnf:/etc/my.cnf -d mysql
--- pro/index.php
+++ pro/index.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+$mysqli = new mysqli("172.20.14.132", "mysql", "mysql","sample",33006); 
+    if(!$mysqli)  { 
+        echo"database error"; 
+    }else{ 
+        echo"php env successful"; 
+    } 
+$mysqli->close(); 
