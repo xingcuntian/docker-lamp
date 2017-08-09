00422fc0e956ce0ba946a2dfbce26cfc2dae769e
xingcuntian <xingcuntian@qq.com>
2017-08-09 18:52:06 +0800

    upgrade

--- php-httpd/composer_start.sh
+++ php-httpd/composer_start.sh
@@ -4,7 +4,7 @@ chown -R developer:docker /var/www
 su developer -c 'composer config -g repo.packagist composer https://packagist.phpcomposer.com'
 su developer -c 'composer global require "laravel/installer=~1.1" '
 su developer -c 'composer create-project laravel/laravel /var/www/laravel --prefer-dist'
-chown -R www-data:docker /var/www
-chown -R www-data:docker /var/www/laravel/storage /var/www/laravel/bootstrap/cache
+chown -R sensetime:docker /var/www
+chown -R sensetime:docker /var/www/laravel/storage /var/www/laravel/bootstrap/cache
 echo "init ok"
 exit 0
